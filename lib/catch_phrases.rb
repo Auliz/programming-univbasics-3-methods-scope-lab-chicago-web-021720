21d845b20ffad01c9b3235507204c2f1fefc400c
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-08 07:01:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+link_phrase = "It's Dangerous To Go Alone! Take this."
+puts link_phrase
+end 
